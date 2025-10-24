82f3f8fd06cb48597a5206d7cf92253d39fb2813
davserpal <davserpal.github@gmail.com>
2025-10-24 20:46:43 +0200

    prueba_crear_modelo

--- PORTDOLIOAPP_DJANGO/app1/models.py
+++ PORTDOLIOAPP_DJANGO/app1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 64, verbose_name = "Name", null = False, blank = False)
+    password = models.CharField(max_length = 64, verbose_name = "Password", null = False, blank = False)
+    email = models.CharField(max_length = 64, verbose_name = "Email", null = False, blank = False)
